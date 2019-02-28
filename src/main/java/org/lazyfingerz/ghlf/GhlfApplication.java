cb749c2d94b098fad55122b9766bd4a838fa3edf
Natalie Grasser <natalie.grasser@daimler.com>
2019-02-28 18:18:06 +0100

    initial commit

---
+++ src/main/java/org/lazyfingerz/ghlf/GhlfApplication.java
@@ -0,0 +1,13 @@
+package org.lazyfingerz.ghlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhlfApplication.class, args);
+	}
+
+}
