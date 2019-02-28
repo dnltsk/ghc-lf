7bf0932872552fc89b23fab0a6ce3a75a0a8886f
Natalie Grasser <natalie.grasser@daimler.com>
2019-02-28 19:47:49 +0100

    getTags via flatMap

--- src/main/java/org/lazyfingerz/ghlf/model/LfSlide.java
+++ src/main/java/org/lazyfingerz/ghlf/model/LfSlide.java
@@ -3,10 +3,18 @@ package org.lazyfingerz.ghlf.model;
 import lombok.Value;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Value
 public class LfSlide {
 
     private List<LfImage> images;
+
+    public List<String> getTags(){
+        return images.stream()
+            .flatMap(i -> i.getTags().stream())
+            .distinct()
+            .collect(Collectors.toList());
+    }
 
 }
